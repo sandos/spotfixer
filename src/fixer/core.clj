5b8a6bba0232fb1d211949e44dc75e1cf750bc1e
unknown <johnba@HIQARBL012.sto.hiq.se>
2012-11-01 16:16:57 +0100

    Get metadata

--- src/fixer/core.clj
+++ src/fixer/core.clj
@@ -18,8 +18,23 @@
 (defn -main
   "I don't do a whole lot."
   [& args]
-  (prn (map #(.substring % 4 26) (into [] (.split (slurp "names") "\n"))))
-  (def a (parse-string (fetch-url (str apiurl "0H7epBAr4BMdBl5NGnUuRr")))))
+  (def a (map #(parse-string (fetch-url (str apiurl %))) 
+              (map 
+                #(.substring % 4 26)
+                (into [] (.split (slurp "names") "\n"))
+                )))
 
+  )
 
 (-main)
+
+
+
+(doseq [x a]
+  (prn (get-in x ["track" "name"])
+       (get-in x ["track" "track-number"])
+       (get-in x ["track" "artists" 0 "name"])
+       (get-in x ["track" "album" "name"])
+       (get-in x ["track" "album" "released"])
+       ))
+
